7fe8f29175135b77a86fe6d36a16ef26e11bbb8f
unknown <akash101811@gmail.com>
2023-07-10 18:38:56 +0530

    changes

--- front/src/App.js
+++ front/src/App.js
@@ -26,8 +26,9 @@ const App =() => {
                         element={!user ? <Auth /> : <Navigate to="/posts" />}
                     />
                     <Route path='/verify' element= {<Verify />}/>
-                    <Route path='/reset' element= {<Reset />}/>
+                    <Route path='/reset/:id' element= {<Reset />}/>
                     <Route path='/reset' element= {<Forgot />}/>
+                    
                 </Routes>
            
         </Container>
